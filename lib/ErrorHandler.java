9575e8163d31866cce9fd9985ed9a748039b78f4
KarinaMendez <karinamendez@Karinas-MacBook-Pro.local>
2014-07-25 08:17:55 -0600

    borre el make 2 y edite el error handler

--- lib/ErrorHandler.java
+++ lib/ErrorHandler.java
@@ -1,1 +1,18 @@
+package compiler.lib;
+
 /*ErrorHandler.java*/
+ 
+
+public class ErrorHandler(){
+
+	public ErrorHandler(String e){
+		Switch(e){
+			case "no arguments":
+				System.out.println("no se recibio ningun argumento");
+				break;
+			default:
+				System.out.println("Error");
+				break;
+		}
+	}
+}

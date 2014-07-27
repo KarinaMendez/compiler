dc60c71b747fa7857f769225d173e25aae6f7463
KarinaMendez <karinamendez@Karinas-MacBook-Pro.local>
2014-07-26 20:31:22 -0600

    manejo de errores por casos

--- lib/ErrorHandler.java
+++ lib/ErrorHandler.java
@@ -8,7 +8,10 @@ public class ErrorHandler(){
 	public ErrorHandler(String e){
 		Switch(e){
 			case "no arguments":
-				System.out.println("no se recibio ningun argumento");
+				System.out.println("Didn't receive any arguments");
+				break;
+			case "not existing file":
+				System.out.println("Not existing file");
 				break;
 			default:
 				System.out.println("Error");

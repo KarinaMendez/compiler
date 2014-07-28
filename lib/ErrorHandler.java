c07ee85f579e5f923a7d47d05e4e8f507c84ee9d
KarinaMendez <karinamendez@Karinas-MacBook-Pro.local>
2014-07-27 23:46:01 -0600

    Modifique verificación de opciones para cuando se recibe mas de una, agregue prints de ayuda al usuario.

--- lib/ErrorHandler.java
+++ lib/ErrorHandler.java
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-package lib;
-/*ErrorHandler.java*/
-=======
 package compiler.lib;
 
 /*ErrorHandler.java*/
- 
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-
 public class ErrorHandler{
 
 	public ErrorHandler(String e){
 		switch(e){
 			case "no arguments":
-<<<<<<< HEAD
 				System.err.println("Didn't receive any arguments");
 				System.exit(0);
 				break;
@@ -40,15 +32,6 @@ public class ErrorHandler{
 			default:
 				System.err.println("Error");
 				System.exit(0);
-=======
-				System.out.println("Didn't receive any arguments");
-				break;
-			case "not existing file":
-				System.out.println("Not existing file");
-				break;
-			default:
-				System.out.println("Error");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 				break;
 		}
 	}

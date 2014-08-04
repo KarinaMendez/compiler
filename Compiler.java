0f14e3560899860dc47a6aa821a958f31d02f0a7
Javier Aguilar <javi.es.32@gmail.com>
2014-08-04 13:37:48 -0600

    Revert "agregue test"
    This reverts commit 8a2f148f5c1c75b8e4aed5ead6f3cbae93cbfa7e.

--- Compiler.java
+++ Compiler.java
@@ -1,39 +1,25 @@
 import java.util.ArrayList;
 <<<<<<< HEAD
-<<<<<<< HEAD
 import lib.ErrorHandler;
 =======
-=======
->>>>>>> origin/Compiler
 import compiler.lib.ErrorHandler;
-/*import compiler.scanner.Scanner;
+import compiler.scanner.Scanner;
 import compiler.parse.CC4Parser;
 import compiler.ast.Ast;
 import compiler.semantic.Semantic;
 import compiler.irt.Irt;
-import compiler.codegen.Codegen;*/
+import compiler.codegen.Codegen;
 
-<<<<<<< HEAD
 
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-=======
->>>>>>> origin/Compiler
 public class Compiler{
 	public static void main(String[] args) {
+
 		if (args.length>0){
-			if (args.length==1){
-				if(args[0].equals("-h")){
-					System.out.println("Esta es una ayuda para el usuario, opciones disponibles: ");
-					System.out.println("-o");
-					System.out.println("-target");
-					System.out.println("-opt");
-					System.out.println("-debug");
-					System.exit(0);
-				}
+			if ( args.length==1){
 				if(args[0].contains(".")){
 					System.out.println("hacer todo");	
 				}else{
-<<<<<<< HEAD
 					//error 
 <<<<<<< HEAD
 					ErrorHandler e = new ErrorHandler("not existing file");
@@ -52,17 +38,10 @@ public class Compiler{
 				System.out.println("not existing file");
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 			}
-=======
-					ErrorHandler e = new ErrorHandler("not existing file");
-				}
-			}
-			String filename = args[args.length-1];
->>>>>>> origin/Compiler
 			ArrayList<String> options = new ArrayList<String>();
 			for(int i = 0;i<=args.length-2;i++){
 				switch(args[i]){
 					case "-o":
-<<<<<<< HEAD
 						if(args[i+1].contains("-")){
 <<<<<<< HEAD
 							ErrorHandler e = new ErrorHandler("no argument for -o");
@@ -86,24 +65,9 @@ public class Compiler{
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -target");
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-=======
-						if(!options.contains(args[i])){
-							options.add(args[i]);
-							if(args[i+1].charAt(0)=='-'){
-								ErrorHandler e = new ErrorHandler("missing -o complement");
-							}else{
-								options.add(args[i+1]);
-								i++;
-							}
-						}else{
-							i++;
->>>>>>> origin/Compiler
 						}
-						break;
-					case "-target":
 						if(!options.contains(args[i])){
 							options.add(args[i]);
-<<<<<<< HEAD
 						}
 						break;
 					case "-opt":
@@ -116,22 +80,9 @@ public class Compiler{
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -opt");
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-=======
-							if(args[i+1].charAt(0)=='-' || args[i+1].contains(".")){
-								ErrorHandler e = new ErrorHandler("missing -target complement");
-							}else{
-								options.add(args[i+1]);
-								i++;
-							}
-						}else{
-							i++;
->>>>>>> origin/Compiler
 						}
-						break;
-					case "-opt":
 						if(!options.contains(args[i])){
 							options.add(args[i]);
-<<<<<<< HEAD
 						}
 						break;
 					case "-debug":
@@ -144,64 +95,63 @@ public class Compiler{
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -debug");
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-=======
-							if(args[i+1].charAt(0)=='-' || args[i+1].contains(".")){
-								ErrorHandler e = new ErrorHandler("missing -opt complement");
-							}else{
-								options.add(args[i+1]);
-								i++;
-							}
-						}else{
-							i++;
->>>>>>> origin/Compiler
 						}
-						break;
-					case "-debug":
 						if(!options.contains(args[i])){
 							options.add(args[i]);
-							if(args[i+1].charAt(0)=='-' || args[i+1].contains(".")){
-								ErrorHandler e = new ErrorHandler("missing -debug complement");
-							}else{
-								options.add(args[i+1]);
-								i++;
-							}
-						}else{
-							i++;
 						}
 						break;
 					default:
-						break;
-				}				
-			}
-			for(int j = 0; options.length()>j;j++){
-				switch(options.get(options.size()-2)){
-					case "-o":
-						System.out.println("el out name sera: " + options.get(j));
-						break;
-					case "-target":
-						System.out.println("se procedera hasta: " + options.get(j));
-						break;
-					case "-opt":
-						System.out.println("solo se optimizara: "+ options.get(j));
-						break;
-					case "-debug":
-						System.out.println("se debugueara: "+ options.get(j));
-						break;
-					case "-h":
-						System.out.println("Muestra esta ayuda al usuario.");
-						break;
-					default:
+						if(!options.contains(args[i])){
+							options.add(args[i]);
+						}
 						break;
 				}
+				for(int j = 0; j<options.size();j++){
+					switch(options.get(j)){
+						case "-o":
+							System.out.println("el out name sera: " + args[j+1]);
+							j++;
+							break;
+						case "-target":
+							String x = args[j+1];
+							System.out.println("se procedera hasta: " + x);
+							switch(x){
+								case "scan":
+								break;
+								case "parse":
+								break;
+								case "ast":
+								break;
+								case "semantic":
+								break;
+								case "irt":
+								break;
+								case "codegen":
+								break;
+							}
+							j++;
+							break;
+						case "-opt":
+							System.out.println("solo se optimizara: "+ args[j+1]);
+							j++;
+							break;
+						case "-debug":
+							System.out.println("se debugueara: "+ args[j+1]);
+							j++;
+							break;
+						case "-h":
+							System.out.println("Muestra esta ayuda al usuario.");
+							break;
+						default:
+							break;
+					}
+				}
+
+				
 			}
 		}else{
-			System.out.println("Esta es una ayuda para el usuario, opciones disponibles: ");
-			System.out.println("-o");
-			System.out.println("-target");
-			System.out.println("-opt");
-			System.out.println("-debug");
-			System.exit(0);
-			//ErrorHandler e = new ErrorHandler("no arguments");
+			//error
+
 		}
 	}
 }
--- lib/ErrorHandler.java
+++ lib/ErrorHandler.java
@@ -1,36 +1,27 @@
-package compiler.lib;
-
+package lib;
 /*ErrorHandler.java*/
+ 
+
 public class ErrorHandler{
 
 	public ErrorHandler(String e){
 		switch(e){
 			case "no arguments":
 <<<<<<< HEAD
-<<<<<<< HEAD
 				System.err.println("no se recibio ningun argumento");
-=======
-				System.err.println("Didn't receive any arguments");
-				System.exit(0);
 				break;
 			case "not existing file":
-				System.err.println("Not existing file");
-				System.exit(0);
->>>>>>> origin/Compiler
+				System.err.println("not existing file");
 				break;
-			case "missing -o complement":
+			case "no argument for -o":
 				System.err.println("error falta el argumento que complementa a -o");
-				System.exit(0);
 				break;
-			case "missing -target complement":
+			case "no argument for -target":
 				System.err.println("error falta el argumento que complementa a -target");
-				System.exit(0);
 				break;
-			case "missing -opt complement":
+			case "no argument for -opt":
 				System.err.println("error falta el argumento que complementa a -opt");
-				System.exit(0);
 				break;
-<<<<<<< HEAD
 			case "not existing file":
 				System.err.println("not existing file");
 =======
@@ -39,15 +30,9 @@ public class ErrorHandler{
 			case "not existing file":
 				System.out.println("Not existing file");
 >>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
-=======
-			case "missing -debug complement":
-				System.err.println("error falta el argumento que complementa a -debug");
-				System.exit(0);
->>>>>>> origin/Compiler
 				break;
 			default:
 				System.err.println("Error");
-				System.exit(0);
 				break;
 		}
 	}

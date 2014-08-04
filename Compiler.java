feed80d6543a00d0529a3ee618f0a8d7a4c352cb
Javier Aguilar <javi.es.32@gmail.com>
2014-08-04 13:42:10 -0600

    holi

--- Compiler.java
+++ Compiler.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
-import lib.ErrorHandler;
-=======
 import compiler.lib.ErrorHandler;
-import compiler.scanner.Scanner;
+/*import compiler.scanner.Scanner;
 import compiler.parse.CC4Parser;
 import compiler.ast.Ast;
 import compiler.semantic.Semantic;
-import compiler.irt.Irt;
+import compiler.irt.Irt;*/
 import compiler.codegen.Codegen;
 
-
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 public class Compiler{
 	public static void main(String[] args) {
 
@@ -21,80 +16,61 @@ public class Compiler{
 					System.out.println("hacer todo");	
 				}else{
 					//error 
-<<<<<<< HEAD
 					ErrorHandler e = new ErrorHandler("not existing file");
-=======
 					System.out.println("not existing file");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 				}
 			}
 			String filename = args[args.length-1];
 			if(!filename.contains(".")){
-<<<<<<< HEAD
 				//error
 				ErrorHandler e = new ErrorHandler("not existing file");
-=======
-				
 				System.out.println("not existing file");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 			}
 			ArrayList<String> options = new ArrayList<String>();
 			for(int i = 0;i<=args.length-2;i++){
 				switch(args[i]){
 					case "-o":
 						if(args[i+1].contains("-")){
-<<<<<<< HEAD
 							ErrorHandler e = new ErrorHandler("no argument for -o");
 						}else{
 							i++;
-=======
 							System.out.println("error falta el argumento que complementa a -o");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 						}
 						if(!options.contains(args[i])){
 							options.add(args[i]);
 						}
 						break;
 					case "-target":
-<<<<<<< HEAD
 						if(args[i+1].contains("-")&&args[i+1].contains(".")){
 							ErrorHandler e = new ErrorHandler("no argument for -target");
 						}else{
 							i++;
-=======
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -target");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 						}
 						if(!options.contains(args[i])){
 							options.add(args[i]);
 						}
 						break;
 					case "-opt":
-<<<<<<< HEAD
 						if(args[i+1].contains("-")&&args[i+1].contains(".")){
 							ErrorHandler e = new ErrorHandler("no argument for -opt");
 						}else{
 							i++;
-=======
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -opt");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 						}
 						if(!options.contains(args[i])){
 							options.add(args[i]);
 						}
 						break;
 					case "-debug":
-<<<<<<< HEAD
 						if(args[i+1].contains("-")&&args[i+1].contains(".")){
 							System.out.print("error falta el argumento que complementa a -debug");
 						}else{
 							i++;
-=======
 						if(args[i+1].contains("-")){
 							System.out.println("error falta el argumento que complementa a -debug");
->>>>>>> e3e91c8b29fcf18162a800bde8eca4511f8cbbec
 						}
 						if(!options.contains(args[i])){
 							options.add(args[i]);
